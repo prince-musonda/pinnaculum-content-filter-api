dc37624c300e1b851ea17126cf1cc321c8173f15
Prince Musonda <princemusonda5840@gmail.com>
2023-12-01 12:16:40 +0200

    updates

--- README.md
+++ README.md
@@ -8,3 +8,10 @@
 ` npm start`
 
 - this will start running the program using pm2( using clusters so as to improve performance)
+
+# How to send a request
+
+- once the program is running, you can send a `POST` request to the root url "/". But ensure that an image file is attached to the resquest with key value of "image"
+- `{image : YOUR IMAGE FILE GOES HERE}`
+
+- For exmple, if the program was running on port 8000 on your local machine, then you would send a post request to `https://localhost:8000/` and with this request, you would attach a request body that contains your `image file` mapped to the key `image`
--- app.js
+++ app.js
@@ -13,6 +13,12 @@ app.post("/", upload.single("image"), (req, res) => {
   // access the image file through req.file.buffer as a readable stream
   const imageFileBuffer = req.file.buffer;
   const originalFileName = req.file.originalname;
+  // return a 404 error if no image is attached
+  if (!imageFileBuffer)
+    return res.status(404).json({
+      status: "failed",
+      message: `couldn't find image or failed to read image`,
+    });
   // detect if the imagefile contains inappropriate content
   contentDetect(imageFileBuffer, originalFileName).then((data) => {
     if ((data.status = "success")) {
@@ -30,7 +36,10 @@ app.post("/", upload.single("image"), (req, res) => {
       console.log(data_to_return);
       return res.json(data_to_return);
     } else {
-      return;
+      // return an an error if image detection failed
+      return res
+        .status(500)
+        .json({ status: "failed", message: "something went wrong" });
     }
   });
 });
